49dbda8c253491502137a84d6535de79aec7c500
shrutik0501 <shrutik0501@gmail.com>
2023-06-29 14:16:04 +0530

    [Shruti]Add:UC8-EmpWageProblem

---
+++ EmpWageProgram/EmpWage_UseCases.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageProgram
+{
+    public  class EmpWage_UseCases
+    {
+        public void Employee_Attendance(string companyName, int wage_Per_Hour,
+            int total_Working_Days, int total_Working_Hours)
+        {
+            int Full_Time_Hours = 8, Part_Time_Hours = 4;
+            int number_of_Working_Hours = 0, number_of_Working_Days = 0;
+            int employee_Wage_for_Month;
+
+            Console.WriteLine("This is the Employee Wage Calculation For " + companyName + " is :");
+            Console.WriteLine();
+
+            Random random = new Random();
+            while (number_of_Working_Days <= total_Working_Days && number_of_Working_Hours <= total_Working_Hours)
+            {
+
+                int Employee = random.Next(3);
+                switch (Employee)
+                {
+                    case 1:
+
+                        Console.WriteLine("Employee is Present for Full Time");
+
+                        number_of_Working_Hours += Full_Time_Hours;
+
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Employee is Present for Part Time");
+
+                        number_of_Working_Days += Part_Time_Hours;
+
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Employee is Absent");
+
+                        break;
+                }
+
+                employee_Wage_for_Month = number_of_Working_Hours * wage_Per_Hour;
+
+                Console.WriteLine("Employee Wage of Employees of  " + companyName + " for a Month is :" +
+                                    " " + employee_Wage_for_Month + " Rupees");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ EmpWageProgram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageProgram
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Wage Computation Program");
+
+            Console.WriteLine("Use_Case-8 for Calculating Employee Wages for Multiple Companies");
+
+            EmpWage_UseCases employeeWage_UseCases = new EmpWage_UseCases();
+
+            employeeWage_UseCases.Employee_Attendance("Suzuki", 1100, 30, 240);
+
+            employeeWage_UseCases.Employee_Attendance("Tata", 1500, 31, 230);
+
+            employeeWage_UseCases.Employee_Attendance("Hyundai", 1105, 29, 215);
+
+            employeeWage_UseCases.Employee_Attendance("Mahindra", 1035, 28, 235);
+
+            Console.ReadLine();
+        }
+    }
+}
+    
+
